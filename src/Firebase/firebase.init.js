d94c1f81af846de07f20b8ac4e9d32a10c25c35e
imRHB <rhbabu3@gmail.com>
2022-01-02 23:05:50 +0600

    firebase init and config added

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAO2CrxKhSbDRzLriqztj1Do5RhqVMdb-0",
+    authDomain: "electro-hub-shop.firebaseapp.com",
+    projectId: "electro-hub-shop",
+    storageBucket: "electro-hub-shop.appspot.com",
+    messagingSenderId: "916850290052",
+    appId: "1:916850290052:web:20fbb758edaa3646bc41d1"
+};
+
+export default firebaseConfig;
