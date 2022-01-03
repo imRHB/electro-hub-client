daa25be516b5ade30fc40bf6faea3355c6c65882
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2022-01-03 18:31:51 +0600

    merge conflict

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Testimonial from './Pages/Testimonials/Testimonial/Testimonial';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/t" element={<Testimonial />} />
-        </Routes>
-      </BrowserRouter>
+      <h2>Welcome to ElectroHub</h2>
     </div>
   );
 }
