3b343abbaeebfaea046b8a83640508bf149048a3
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2022-01-04 02:04:41 +0600

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './Pages/Home/Home/Home';
 import NotFound from './Pages/NotFound/NotFound';
 import Products from './Pages/Products/Products/Products';
 import About from './Pages/About/About';
-// import './App.css';
+
 
 function App() {
   return (
