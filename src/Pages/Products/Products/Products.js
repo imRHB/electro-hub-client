2b15196b88be7c0232417aed0878150385e33203
imRHB <rhbabu3@gmail.com>
2022-01-04 02:48:27 +0600

    product and single product demo added

--- src/Pages/Products/Products/Products.js
+++ src/Pages/Products/Products/Products.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container, Row } from "react-bootstrap";
+import SingleProduct from "../SingleProduct/SingleProduct";
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
     return (
         <div>
-            <h3>this is products</h3>
+            <Container>
+                <h2 className="text-center fs-1 fw-bold text-secondary my-5">Available Products</h2>
+                <Row xs={1} md={2} lg={3} xl={3} className="g-4">
+                    {
+                        products.map(product => <SingleProduct
+                            key={product._id}
+                            product={product}
+                        ></SingleProduct>)
+                    }
+                </Row>
+            </Container>
         </div>
     );
 };
