53a9c1731b8eb3ef9323c0aaba30dd51f7b8230d
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2022-01-03 13:05:49 +0600

    routing problem solved

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className="">
       <Router>
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
