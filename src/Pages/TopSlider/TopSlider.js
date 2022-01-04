7d9f8b5805f6fb7dc28095bdd786a52ef06ae6ef
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2022-01-04 20:42:08 +0600

    carousel

--- src/Pages/TopSlider/TopSlider.js
+++ src/Pages/TopSlider/TopSlider.js
@@ -10,7 +10,7 @@ const TopSlider = () => {
                 <Carousel.Item>
                     <img
                         className="d-block w-100 slider-img"
-                        src="https://i.ibb.co/ZXdjpKV/oxana-melis-Bu-Auu-EI2ub-M-unsplash.jpg"
+                        src="https://i.ibb.co/X4mZZVp/Dark-style-realistic-smart-watch-horizontal-banner-with-for-advertising-on-site-vector-illustration.jpg"
                         alt="First slide"
                     />
                     {/* <Carousel.Caption className="caption">
@@ -19,18 +19,18 @@ const TopSlider = () => {
                         <Button className="text-white border border-2 px-4 py-2 slider-btn" variant="outside-light">Prebook Now <i className="fas fa-angle-double-right"></i></Button>
                     </Carousel.Caption> */}
                 </Carousel.Item>
-                {/* <Carousel.Item>
+                <Carousel.Item>
                     <img
                         className="d-block w-100 slider-img"
-                        src="https://i.ibb.co/vxt79pk/harley-davidson-ee-TJKC-wz34-unsplash.jpg"
+                        src="https://i.ibb.co/rHXZvWT/Vector-realistic-mockup-with-black-portable-smart-speaker-on-gray-background-Wireless-audio-device-w.jpg"
                         alt="Second slide"
                     />
 
-                    <Carousel.Caption className="caption">
+                    {/* <Carousel.Caption className="caption">
                         <h1>Latest Offers</h1>
                         <p className="fs-4">Upto 15% discount is ongoing</p>
                         <Button className="text-white border border-2 px-4 py-2 slider-btn" variant="outside-light">Order Now <i className="fas fa-angle-double-right"></i></Button>
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -39,11 +39,11 @@ const TopSlider = () => {
                         alt="Third slide"
                     />
 
-                    <Carousel.Caption className="caption">
+                    {/* <Carousel.Caption className="caption">
                         <h1>SOmething cooming</h1>
                         <p className="fs-4">We have best and unique bikes.</p>
                         <Button className="text-white border border-2 px-4 py-2 slider-btn" variant="outside-light">Order Now <i className="fas fa-angle-double-right"></i></Button>
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
@@ -52,12 +52,12 @@ const TopSlider = () => {
                         alt="Third slide"
                     />
 
-                    <Carousel.Caption className="caption">
+                    {/* <Carousel.Caption className="caption">
                         <h1>Largest Motorcycle Store</h1>
                         <p className="fs-4">All kinds of Bikes is available here.</p>
                         <Button className="text-white border border-2 px-4 py-2 slider-btn" variant="outside-light">Order Now <i className="fas fa-angle-double-right"></i></Button>
-                    </Carousel.Caption>
-                </Carousel.Item> */}
+                    </Carousel.Caption> */}
+                </Carousel.Item>
             </Carousel>
         </div>
     );
