886f4e753e1d1aa0b1be6de77332d4e26d65242e
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2022-01-05 20:25:43 +0600

    minor changes

--- src/Pages/shopFacilities/shopFacilities.js
+++ src/Pages/shopFacilities/shopFacilities.js
@@ -4,30 +4,26 @@ import styles from './ShopFacilities.module.css';
 
 const facilities = [
     {
-        id: "s1001",
         image: "https://i.ibb.co/X5HCX9k/shipping-free-512-grande.png",
         status: "FREE SHIPPING",
         desc: "Free shipping on all order"
     },
     {
-        id: "s1002",
         image: "https://i.ibb.co/jGQSsD5/80630.png",
         status: "ONLINE SUPPORT",
         desc: "Online support 24 hours a day"
     },
     {
-        id: "s1003",
         image: "https://i.ibb.co/z5Pr6ds/3427751.png",
         status: "MONEY RETURN",
         desc: "Back guarantee under 7 days"
     },
     {
-        id: "s1004",
         image: "https://i.ibb.co/KNsZLhN/87255.png",
         status: "MEMBER DISCOUNT",
         desc: "On every order over $300"
     },
-]
+];
 
 const ShopFacilities = () => {
     return (
@@ -41,7 +37,7 @@ const ShopFacilities = () => {
                 <div className="py-5 px-auto mx-auto">
                     <Row xs={1} md={2} lg={4}>
                         {
-                            facilities?.map(facility => <div key={facility?.id}>
+                            facilities?.map((facility, f_id) => <div key={f_id}>
                                 <Col className="mx-auto my-3">
                                     <div className={`d-flex align-items-center justify-content-center flex-column mx-auto p-3 text-white rounded-3 ${styles.status}`}>
                                         <img className="img-fluid mx-auto" width="30%" src={facility?.image} alt="" />
