ba61db44bcd9606bfec16ce0e64f1819d1581031
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2022-01-05 17:51:40 +0600

    carousel issues trying to fix

--- src/Pages/TopSlider/TopSlider.js
+++ src/Pages/TopSlider/TopSlider.js
@@ -73,6 +73,69 @@ const TopSlider = () => {
 
                 </Carousel.Item>
             </Carousel> */}
+
+
+
+            <Carousel className={`${styles.slider}`}>
+                <Carousel.Item className={`${styles.sliderItem}`}>
+                    <img
+                        className={`d-block w-100 ${styles.sliderImg}`}
+                        src="https://i.ibb.co/wShSkB4/laptop.jpg"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption className={`${styles.caption}`}>
+                        <h1>Upcoming Laptop
+                            <Typical
+                                steps={[' MacBook Pro', 1000, ' HP Envy x360', 1000, ' Razer Book 13', 1000]}
+                                loop={Infinity}
+                                wrapper="p"
+                            />
+
+                        </h1>
+                        <p className="fs-4 ">Prebook is gooing on</p>
+                        <Button className={`text-white border border-2 px-4 py-2 ${styles.sliderBtn}`} variant="outside-light">Prebook Now <i className="fas fa-angle-double-right"></i></Button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className={`${styles.sliderItem}`}>
+                    <img
+                        className={`d-block w-100 ${styles.sliderImg}`}
+                        src="https://i.ibb.co/9yHDsVk/iphone.jpg"
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption className={`${styles.caption}`}>
+                        <h1>Latest Offers
+                            <Typical
+                                steps={[' In Mobile Phones', 1000, ' and Computers ', 1000]}
+                                loop={Infinity}
+                                wrapper="b"
+                            />
+                        </h1>
+                        <p className="fs-4">Upto 15% discount is ongoing</p>
+                        <Button className={`text-white border border-2 px-4 py-2 ${styles.sliderBtn}`} variant="outside-light">Order Now <i className="fas fa-angle-double-right"></i></Button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item className={`${styles.sliderItem}`}>
+                    <img
+                        className={`d-block w-100 ${styles.sliderImg}`}
+                        src="https://i.ibb.co/6cQFSnb/speaker.jpg"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption className={`${styles.caption}`}>
+                        <h1>
+                            <Typical
+                                steps={['Best Collections', 1000, 'Best Service', 1000, 'Best Price', 1000]}
+                                loop={Infinity}
+                                wrapper="b"
+                            />
+                        </h1>
+                        <p className="fs-4">We have best and unique bikes.</p>
+                        <Button className={`text-white border border-2 px-4 py-2 ${styles.sliderBtn}`} variant="outside-light">Order Now <i className="fas fa-angle-double-right"></i></Button>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+            </Carousel>
         </div>
     );
 };
