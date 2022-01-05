bacaa35e21bd17a5f8206ab1673e4ab473e0adf9
champmahfuz <mahfuzrahman517@gmail.com>
2022-01-06 02:37:26 +0600

    added register function

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Register.module.css';
 import img1 from './img/img-login.svg';
-import { NavLink } from 'react-router-dom';
-
+import { NavLink, useNavigate } from 'react-router-dom';
+import useAuth from '../../Hook/useAuth';
 const Register = () => {
+    const [loginData, setLoginData] = useState({});
+    const history = useNavigate();
+    const { user, registerUser, isLoading, authError } = useAuth();
+
+    const handleOnBlur = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+    }
+
+    const handleLoginSubmit = e => {
+        if (loginData.password !== loginData.password2) {
+            alert('Your password did not match');
+            return
+        }
+        registerUser(loginData.email, loginData.password, loginData.name, history);
+        e.preventDefault();
+    }
     return (
         <>
             <div className={`${styles.login}`}>
@@ -14,7 +34,7 @@ const Register = () => {
 
                     <div className={`${styles.loginForms}`}>
 
-                        <form action="" className={`${styles.loginRegister}`} id="login-in">
+                        <form onSubmit={handleLoginSubmit} className={`${styles.loginRegister}`} id="login-in">
                             <h1 className={`${styles.loginTitle}`}>Please Register</h1>
 
                             {/* 
@@ -49,20 +69,20 @@ const Register = () => {
 
                             <div className={`${styles.loginBox}`}>
 
-                                <input type="text" placeholder="Name" className={`${styles.loginInput}`} />
+                                <input type="text" placeholder="Name" onChange={handleOnBlur} className={`${styles.loginInput}`} />
                             </div>
                             <div className={`${styles.loginBox}`}>
 
-                                <input type="text" placeholder="Email" className={`${styles.loginInput}`} />
+                                <input type="text" placeholder="Email" onChange={handleOnBlur} className={`${styles.loginInput}`} />
                             </div>
 
                             <div className={`${styles.loginBox}`}>
 
-                                <input type="password" placeholder="Password" className={`${styles.loginInput}`} />
+                                <input type="password" placeholder="Password" onChange={handleOnBlur} className={`${styles.loginInput}`} />
                             </div>
                             <div className={`${styles.loginBox}`}>
 
-                                <input type="password" placeholder="Re-type your Password" className={`${styles.loginInput}`} />
+                                <input type="password" placeholder="Re-type your Password" onChange={handleOnBlur} className={`${styles.loginInput}`} />
                             </div>
 
                             <NavLink to="/login" className={`${styles.loginForgot}`}>Already have an Account?Please Login</NavLink>
