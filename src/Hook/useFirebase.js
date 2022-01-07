5fb29188e25547951479f5d4466791f229f0e599
imRHB <rhbabu3@gmail.com>
2022-01-07 19:37:36 +0600

    fixed google save user to database

--- src/Hook/useFirebase.js
+++ src/Hook/useFirebase.js
@@ -68,6 +68,7 @@ const useFirebase = () => {
 
         signInWithPopup(auth, googleProvider)
             .then((result) => {
+                const user = result?.user;
                 const destination = location?.state?.from || '/';
                 //save user to database
                 saveUser(user?.email, user?.displayName, 'PUT');
